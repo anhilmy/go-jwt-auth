0b2f515cd27e0980a6fea9c4ac089584d990059c
anhilmy <hilmyahmadnaufal@gmail.com>
2023-10-09 20:16:24 +0700

    fix: change column length

--- migrations/000001_init_user_table.up.sql
+++ migrations/000001_init_user_table.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS users(
     id serial PRIMARY KEY ,
     username VARCHAR (50) UNIQUE NOT NULL,
     email VARCHAR (300) UNIQUE NOT NULL,
-    password VARCHAR (50) NOT NULL
+    password VARCHAR (60) NOT NULL
 );         
