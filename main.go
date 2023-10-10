6a0a8c9230c2b6be38462b3e3e43750e10ed56a7
anhilmy <hilmyahmadnaufal@gmail.com>
2023-10-10 18:27:28 +0700

    inject config file

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"go-jwt-auth/database"
 	"go-jwt-auth/internal/auth"
 	"go-jwt-auth/internal/config"
+	"go-jwt-auth/internal/token"
 	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
-var flagConfig = flag.String("config", "./.local.yml", "config file path")
+var flagConfig = flag.String("config", ".local.yml", "config file path")
 
 func main() {
 	r := gin.Default()
@@ -27,13 +28,13 @@ func main() {
 		log.Fatal("Cannot open database")
 	}
 
-	registerHandler(r, db)
+	registerHandler(r, db, config)
 
 	r.Run()
 
 }
 
-func registerHandler(router *gin.Engine, db *database.Database) {
+func registerHandler(router *gin.Engine, db *database.Database, config *config.Config) {
 	api := router.Group("/api")
 
 	v1 := api.Group("/v1")
@@ -42,7 +43,8 @@ func registerHandler(router *gin.Engine, db *database.Database) {
 	userRepo := auth.NewRepository(db)
 
 	// init serv
-	authServ := auth.NewService(userRepo)
+	tokenServ := token.NewService(config)
+	authServ := auth.NewService(userRepo, tokenServ)
 
 	auth.Handler(v1, authServ)
 
