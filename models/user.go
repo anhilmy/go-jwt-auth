77ca969eeb9a6f74099720ce9772c89e67a1e7c6
anhilmy <hilmyahmadnaufal@gmail.com>
2023-10-10 18:26:40 +0700

    feat: add hashing and verify password

--- models/user.go
+++ models/user.go
@@ -1,5 +1,7 @@
 package models
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
 	ID       int    `db:"id"`
 	Username string `db:"username"`
@@ -9,4 +11,18 @@ type User struct {
 
 func (mod User) TableName() string {
 	return "users"
+}
+
+func (m *User) HashPassword() error {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(m.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	m.Password = string(hashed)
+	return nil
+}
+
+func (m User) VerifyPassword(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(m.Password), []byte(password))
 }
